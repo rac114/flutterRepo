3607e22de50b95361eca6d21001db7c676ba20e3
rac114 <joinhc04@gmail.com>
2025-08-24 01:18:58 +0900

    second_page.dart 업데이트

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -3,41 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:installed_apps/app_info.dart';
 import 'package:installed_apps/installed_apps.dart';
 
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text('이전 페이지로 돌아가기'),
-          onPressed: () {
-            // Navigator를 사용하여 이전 페이지로 돌아감
-            Navigator.pop(context);
-          },
-        ),
-      ),
-    );
-  }
-}
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Home Launcher',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const AppListScreen(),
-    );
-  }
-}
-
-class AppListScreen extends StatefulWidget {
-  const AppListScreen({Key? key}) : super(key: key);
+class SecondPage extends StatefulWidget {
+  const SecondPage({Key? key}) : super(key: key);
 
   @override
   _AppListScreenState createState() => _AppListScreenState();
@@ -60,7 +27,15 @@ class _AppListScreenState extends State<AppListScreen> {
       appBar: AppBar(
         title: const Text('Installed Apps'),
       ),
-      body: FutureBuilder<List<AppInfo>>(
+      body: Center(
+        child: ElevatedButton(
+          child: Text('이전 페이지로 돌아가기'),
+          onPressed: () {
+            // Navigator를 사용하여 이전 페이지로 돌아감
+            Navigator.pop(context);
+          },
+        ),
+      ), FutureBuilder<List<AppInfo>>(
         future: _apps,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -79,10 +54,10 @@ class _AppListScreenState extends State<AppListScreen> {
                     InstalledApps.startApp(app.packageName);
                   },
                   child: ListTile(
-														  	leading: if (app.icon != null) {Image.memory(app.icon!, width: 50, height:50),
-																title: Text(app.name!),
-																subtitle: const Text('list subtitle'),	
-														),
+                    leading: if (app.icon != null) {Image.memory(app.icon!, width: 50, height:50),
+                    title: Text(app.name!),
+                    subtitle: const Text('list subtitle'),	
+                  ),
                 );
               },
             );
