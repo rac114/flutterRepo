0310dd9c2ea1a6f19f84d196cd4ec1e758699624
rac114 <joinhc04@gmail.com>
2025-08-24 00:08:19 +0900

    main.dart 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/first_page.dart';
 import 'package:myapp/second_page.dart';
 
 void main() {
@@ -11,6 +10,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: FirstPage(), // 앱의 시작 페이지
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('첫 번째 페이지!!!'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('다음 페이지로 이동'),
+          onPressed: () {
+            // Navigator를 사용하여 SecondPage로 이동
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+        ),
+      ),
     );
   }
 }
