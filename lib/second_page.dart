c0eec43481701691ef228124a28c9e1d7895b8eb
rac114 <joinhc04@gmail.com>
2025-08-24 02:03:31 +0900

    second_page.dart 업데이트

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -33,40 +33,42 @@ class _SecondPageState extends State<SecondPage> {
           ElevatedButton(
             child: Text('이전 페이지로 돌아가기'),
             onPressed: () {
-            // Navigator를 사용하여 이전 페이지로 돌아감
-            Navigator.pop(context);
+              Navigator.pop(context);
             },
           ),
-          FutureBuilder<List<AppInfo>>(
-            future: _apps,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return const Center(child: Text('No apps found.'));
-              } else {
-                return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    final app = snapshot.data![index];
-                    return GestureDetector(
-                      onTap: () {
-                        InstalledApps.startApp(app.packageName);
-                      },
-                      child: ListTile(
-                        leading: Image.memory(app.icon!, width: 50, height:50),
-                        title: Text(app.name!),
-                        subtitle: const Text('list subtitle'),        
-                      ),
-                    );
-                  },
-                );
-              }
-            },
+          // FutureBuilder를 Expanded로 감싸서 남은 공간을 모두 차지하도록 함
+          Expanded(
+            child: FutureBuilder<List<AppInfo>>(
+              future: _apps,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('No apps found.'));
+                } else {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      final app = snapshot.data![index];
+                      return GestureDetector(
+                        onTap: () {
+                          InstalledApps.startApp(app.packageName);
+                        },
+                        child: ListTile(
+                          leading: Image.memory(app.icon!, width: 50, height: 50),
+                          title: Text(app.name!),
+                          subtitle: const Text('list subtitle'),
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
           ),
-        ]
+        ],
       ),
     );
   }
