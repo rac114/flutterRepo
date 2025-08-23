84b12a9a5d23da6478a0c8645bb223f2bbb521bc
rac114 <joinhc04@gmail.com>
2025-08-24 02:32:49 +0900

    main.dart 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: FirstPage(), // 앱의 시작 페이지
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.lightGreen
+        scaffoldBackgroundColor: const Color.fromRGBO(243,239,230,1)
       ),
     );
   }
@@ -21,9 +21,6 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('첫 번째 페이지!!!'),
-      ),
       body: Center(
         child: ElevatedButton(
           child: Text('다음 페이지로 이동'),
