4d8ac27e5a4052bfaec0910007701571a39a60db
rac114 <joinhc04@gmail.com>
2025-08-26 00:13:16 +0900

    main.dart 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 import 'package:myapp/second_page.dart';
 
@@ -31,6 +32,81 @@ class FirstPage extends StatelessWidget {
               MaterialPageRoute(builder: (context) => SecondPage()),
             );
           },
+        ),
+      ),
+    );
+  }
+}
+*/
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyDraggableContainer(),
+    );
+  }
+}
+
+class MyDraggableContainer extends StatefulWidget {
+  @override
+  _MyDraggableContainerState createState() => _MyDraggableContainerState();
+}
+
+class _MyDraggableContainerState extends State<MyDraggableContainer> {
+  Color _containerColor = Colors.blue;
+  String _message = '여기로 드래그하세요';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Draggable Container')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            // 드래그 가능한 컨테이너
+            Draggable<Color>(
+              data: Colors.red, // 드래그 시 전달할 데이터
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+                child: Center(child: Text('드래그하세요')),
+              ),
+              feedback: Container(
+                width: 100,
+                height: 100,
+                color: Colors.red.withOpacity(0.5),
+                child: Center(child: Text('드래그 중')),
+              ),
+            ),
+            
+            // 드래그된 컨테이너가 놓일 곳
+            DragTarget<Color>(
+              onWillAccept: (data) => data == Colors.red,
+              onAccept: (data) {
+                setState(() {
+                  _containerColor = data;
+                  _message = '드래그 성공!';
+                });
+              },
+              builder: (context, candidateData, rejectedData) {
+                return Container(
+                  width: 150,
+                  height: 150,
+                  color: _containerColor,
+                  child: Center(child: Text(_message)),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
