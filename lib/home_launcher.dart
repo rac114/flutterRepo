48add1d4273801d31d741d8f6a6ad5fa90974a2c
rac114 <joinhc04@gmail.com>
2025-08-26 23:39:43 +0900

    home_launcher.dart 업데이트

--- lib/home_launcher.dart
+++ lib/home_launcher.dart
@@ -1,2 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/installed_apps.dart';
+import 'package:installed_apps/installed_apps.dart';
+import 'package:installed_apps/app_info.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class homeLauncher extends StatefulWidget {
+  const homeLauncher({Key? key}) : super(key:key);
+
+  @override
+  _homeLauncherState createState() => _homeLauncherState();
+}
+
+class _homeLauncherState extends State<HomeLauncher> {
+
+}
+
+
+class homePageButton extends StatefulWidget {
+
+}
+
+class homePageButtonState extends State<homePageButton> {
+
+}
